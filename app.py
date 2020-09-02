41c511302a1e95ae409b31a3da65cabf711e5d53
kaochinchang@gmail.com <kaochinchang@gmail.com>
2020-09-03 00:43:29 +0800

    deploy app.py

--- app.py
+++ app.py
@@ -137,4 +137,4 @@ def sendMail(id):
         return render_template('send.html',contact=contact,emails=emails)
 
 if __name__ == '__main__' :
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
